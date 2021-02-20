473262d9127c59c8a2671653f61671f81971fe10
Kensley92 <72991374+Kensley92@users.noreply.github.com>
2021-02-19 19:24:05 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const TodoList = () => {
+const posts = () => {
     const [posts, setPosts] = useState();
 
     useEffect (() => {
         axios.get('https://jsonplaceholder.typicode.com/posts').then((res0) => {
-            const setPosts = res.data;
-            setPosts(setPosts);
+            const responsePosts = res.data;
+            setPosts(responsePosts);
         });
     }, []);
     return (
         <div>
-           <h1>To Do List</h1>
+            <h1>Posts</h1>
             {posts &&
             posts.map((todo) => {
-            const {id, title} = posts;
-            return (
-              <div key={id}>
-                <h2>{title}</h2>
-                <h3>Information: {id}</h3>
-              </div>
+                const {id, title} = posts;
+                return (
+                    <div key={id}>
+                        <h5>{title}</h5>
+                        <h6>Posts: {id}</h6>
+                    </div>
                 )
             })}
         </div>
